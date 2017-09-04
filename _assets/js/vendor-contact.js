0de761f328c61c33dbd14278ebb85271b75c1561
Kyle Kirkby <kyle.kirkby@linaro.org>
2017-09-04 12:26:20 +0100

    Updated members.html to use assets

--- _assets/js/vendor-contact.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/jquery.min
-//= require vendor/bootstrap.min
-//= require vendor/cookieconsent.min
-//= require vendor/lazysizes.min
-//= require vendor/cognito-forms
-
-//= require app/main
-//= require app/tables
-//= require vendor/mc-validate
