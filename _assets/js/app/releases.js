0acb8bc9cf7c7102d0beea61c051e5fea947d5f5
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-01-09 14:15:34 +0000

    Updated the custom.scss file to include pre-release style

--- _assets/js/app/releases.js
+++ _assets/js/app/releases.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
     var monthNames = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"
     ];
+
     var latest_release_span = "<span class='latest-release'>Latest Release</span>";
-    var prerelease_span = "<span class='latest-release'>Latest Release</span>";
+    var prerelease_span = "<span class='pre-release'>Pre-release</span>";
 
 
     $.get(api, function (data) {
@@ -15,27 +16,27 @@ $(document).ready(function(){
           
            var release_date = new Date(obj.published_at);
            console.log(idx);
-           var formatted_release_date = release_date.getDate() + " " + monthNames[release_date.getMonth()] + release_date.getFullYear();
+           var formatted_release_date = release_date.getDate() + " " + monthNames[release_date.getMonth()] + " " + release_date.getFullYear();
            
            // Check to see if it is the latest release
            if (idx == 0){
                // Check to see if the tag name exists
-               if(obj.tag_name){
                    if(obj.prerelease == true){
-                       $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p>");
+                       $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + prerelease_span + " " + latest_release_span + "</p>");
                    }
                    else{
                        $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p>");
 
-                   }
-               }
-               else{
-                   $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p> ");
-               }     
+                   }               
            }
            else
            {
+               if(obj.prerelease == true){
+                   $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + prerelease_span +  "</p>");
+               }
+               else{
                    $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + "</p>");
+               }  
             }     
           
 
