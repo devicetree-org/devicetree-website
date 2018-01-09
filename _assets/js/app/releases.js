80eef63283827fc56223b6a5ba69866444649cb4
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-01-09 13:00:53 +0000

    Added a releases.js file which is used to gather the releases json from api.github.com

---
+++ _assets/js/app/releases.js
@@ -0,0 +1,49 @@
+// Handles the gathering of github  releases details.
+
+$(document).ready(function(){
+    var api = "https://api.github.com/repos/devicetree-org/devicetree-specification/releases";
+    
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    var latest_release_span = "<span class='latest-release'>Latest Release</span>";
+    var prerelease_span = "<span class='latest-release'>Latest Release</span>";
+
+
+    $.get(api, function (data) {
+       $.each(data, function (idx, obj) {
+          
+           var release_date = new Date(obj.published_at);
+           console.log(idx);
+           var formatted_release_date = release_date.getDate() + " " + monthNames[release_date.getMonth()] + release_date.getFullYear();
+           
+           // Check to see if it is the latest release
+           if (idx == 0){
+               // Check to see if the tag name exists
+               if(obj.tag_name){
+                   if(obj.prerelease == true){
+                       $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p>");
+                   }
+                   else{
+                       $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p>");
+
+                   }
+               }
+               else{
+                   $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + " " + latest_release_span + "</p> ");
+               }     
+           }
+           else
+           {
+                   $("#devicetree_releases").append("<p class='devicetree_release'><a href='" + obj.html_url + "'>" + obj.tag_name + "</a> - Released " + formatted_release_date + "</p>");
+            }     
+          
+
+       });
+   });
+    
+});
+
+
+
+
