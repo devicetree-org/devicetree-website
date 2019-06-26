49652d4f15bd496db88da4f3f74a51c0c3b231f0
Philip Colmer <1979931+pcolmer@users.noreply.github.com>
2019-06-26 09:53:11 +0100

    Stop altering GEM_HOME (#60)

--- build-site.sh
+++ build-site.sh
@@ -9,13 +9,10 @@ if [ -z "$JEKYLL_ENV" ]; then
 fi
 export JEKYLL_CONFIG="_config.yml,_config-$JEKYLL_ENV.yml"
 
-if [ -z "$GEM_HOME" ]; then
-        export GEM_HOME="$(pwd)/.gems"
-fi
-if [ ! -d "$GEM_HOME" ]; then
-        mkdir "$GEM_HOME"
+if [ ! -d "$(pwd)/.gems" ]; then
+        mkdir "$(pwd)/.gems"
 fi
 
 docker run --rm -it -p 4000:4000 -e JEKYLL_ACTION -e JEKYLL_CONFIG -e JEKYLL_ENV \
         -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -v "$HOME":/srv/home \
-        -v "$GEM_HOME:/gems" -v "$(pwd)":/srv/source linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" build-site.sh
+        -v "$(pwd)/.gems:/gems" -v "$(pwd)":/srv/source linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" build-site.sh
