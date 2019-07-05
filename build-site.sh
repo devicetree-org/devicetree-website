8519ed5012074fe01635cdf8d9b4f2b3017742cf
Philip Colmer <1979931+pcolmer@users.noreply.github.com>
2019-07-05 09:36:40 +0100

    Update to latest build spec (#71)
    * Update to latest build spec
    
    * Remove -i flag

--- build-site.sh
+++ build-site.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 if [ -z "$JEKYLLSITEBUILD" ]; then
-        export JEKYLLSITEBUILD=latest
+  export JEKYLLSITEBUILD=latest
 fi
 
 if [ -z "$JEKYLL_ENV" ]; then
-	export JEKYLL_ENV=staging
-fi
-export JEKYLL_CONFIG="_config.yml,_config-$JEKYLL_ENV.yml"
-
-if [ ! -d "$(pwd)/.gems" ]; then
-        mkdir "$(pwd)/.gems"
+  export JEKYLL_ENV=staging
 fi
 
-docker run --rm -it -p 4000:4000 -e JEKYLL_ACTION -e JEKYLL_CONFIG -e JEKYLL_ENV \
-        -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" -v "$HOME":/srv/home \
-        -v "$(pwd)/.gems:/gems" -v "$(pwd)":/srv/source linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" build-site.sh
+docker run \
+  --cap-drop ALL \
+  --rm \
+  -t \
+  -p 4000:4000 \
+  -e JEKYLL_ACTION \
+  -e JEKYLL_ENV \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -u "$(id -u)":"$(id -g)" \
+  -v "$(pwd)":/srv/source \
+  linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" \
+  build-site.sh
