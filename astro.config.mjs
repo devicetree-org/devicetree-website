4b9106eaac99141651ff72a7804be036d666ea45
Louis Morgan <louis.morgan@linaro.org>
2023-10-19 16:29:08 +0100

    add 404, sitemap, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: "https://www.devicetree.org/",
   integrations: [
     sitemap(),
     tailwind({
